a7396a38b6b05b0ca1daff08b7ecf41fc37e95d1
alissone <damascenoalisson@gmail.com>
2025-03-19 23:37:44 -0300

    Add pick image button

--- app/src/main/java/com/example/imagetor/SecondActivity.kt
+++ app/src/main/java/com/example/imagetor/SecondActivity.kt
@@ -1,14 +1,37 @@
-
 package com.example.imagetor
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.example.imagetor.databinding.SecondActivityBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: SecondActivityBinding
+
+    private val pickImageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val imageUri: Uri? = data?.data
+            binding.imageView.setImageURI(imageUri)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
         enableEdgeToEdge()
-        setContentView(R.layout.second_activity) // Create this layout file
+        binding = SecondActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pickImageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            pickImageLauncher.launch(intent)
+        }
     }
 }
